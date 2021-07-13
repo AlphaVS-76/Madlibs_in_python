8e9e2b12779511020a9de90e2ace3fc6144968af
AlphaVS-76 <76773190+AlphaVS-76@users.noreply.github.com>
2021-07-13 08:58:28 +0530

    Game of madlibs in python
    Playing MadLibs using python to take adjectives/verbs/nouns/plural nouns from the user

---
+++ madlib.py
@@ -0,0 +1,24 @@
+#madlibs game in python
+
+adj1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+f1 = input("Enter a drink: ")
+f2 = input("Enter a food article: ")
+verb2 = input("Enter a verb: ")
+pnoun = input("Enter a plural noun: ")
+
+print("\n")
+
+print("Let's play a game of MadLibs!!!\n")
+print("Summer trips to the beach are so")
+print(adj1 + " ! Pack your bags, a")
+print(noun1 + " to dry yourself off, and")
+print(noun2 + " to prevent sunburn. Be sure")
+print(" to bring a " + noun3 + " to " + verb1 + " with")
+print("in the water, too. You can bring a " + f1)
+print("to drink and " + f2 + " to eat. It's")
+print("fun to " + verb2 + " for hours in the water, ")
+print("and to see " + pnoun + " sail past in the distance.")
